d8d0d9ec978675ab2fc86c59cbce59e442c43d36
Victor Henrique Ventura Freitas <79233369+VhCompany1@users.noreply.github.com>
2021-06-17 16:04:24 -0300

    1.1.2

---
+++ app/src/components/pages/header.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function header(){
+    return(
+        <div id='header'>
+            <a id='title'>HAYS</a> <p><a>HOME</a> <a>ABOUT US</a> <a>CONTACT</a></p>
+        </div>
+    )
+}
