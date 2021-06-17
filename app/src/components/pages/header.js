6c0643d0484764eb16fe05879602ac5ca1db58da
Victor Henrique Ventura Freitas <79233369+VhCompany1@users.noreply.github.com>
2021-06-17 16:25:44 -0300

    1.1.3

--- app/src/components/pages/header.js
+++ app/src/components/pages/header.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function header(){
     return(
         <div id='header'>
-            <a id='title'>HAYS</a> <p><a>HOME</a> <a>ABOUT US</a> <a>CONTACT</a></p>
+            <span id='title'>HAYS</span> <span id='link'><a>HOME</a> <a>ABOUT US</a> <a>CONTACT</a></span>
         </div>
     )
 }
